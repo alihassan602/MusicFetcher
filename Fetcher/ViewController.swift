029e477ec8f0238a2ab9c0884ce5e3b6324da185
Ali Hassan <ali.hassan@fanduel.com>
2020-06-28 11:28:05 +0100

    Initial Commit

---
+++ Fetcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Fetcher
+//
+//  Created by Ali Hassan on 28/06/2020.
+//  Copyright © 2020 Ali Hassan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
