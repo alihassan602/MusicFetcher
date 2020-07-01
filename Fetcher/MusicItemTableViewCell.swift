0c15f9219f3e5bcfffdfa0d0ea4949003a76bfd7
Ali Hassan <ali.hassan@fanduel.com>
2020-07-01 17:00:59 +0100

    initial commit

---
+++ Fetcher/MusicItemTableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  MusicItemTableViewCell.swift
+//  Fetcher
+//
+//  Created by Ali Hassan on 28/06/2020.
+//  Copyright © 2020 Ali Hassan. All rights reserved.
+//
+
+import Foundation
---
+++ Fetcher/Presenter.swift
@@ -0,0 +1,9 @@
+//
+//  Presenter.swift
+//  Fetcher
+//
+//  Created by Ali Hassan on 29/06/2020.
+//  Copyright © 2020 Ali Hassan. All rights reserved.
+//
+
+import Foundation
